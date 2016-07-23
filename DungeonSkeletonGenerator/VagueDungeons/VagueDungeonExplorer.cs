12114acd3bb545676f149421c99915fc4f94cb20
Alex Shelley <ashelley@purdue.edu>
2016-07-23 17:43:07 -0400

    Started on VagueDungeonExplorer, an agent that moves around a dungeon.

---
+++ DungeonSkeletonGenerator/VagueDungeons/VagueDungeonExplorer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonSkeletonGenerator.VagueDungeons
+{
+    public class VagueDungeonExplorer
+    {
+        public VagueDungeonGraph dungeon { get; private set; }
+        public VagueDungeonNode currentRoom { get; private set; }
+
+        public VagueDungeonExplorer(VagueDungeonGraph dungeon)
+        {
+            this.dungeon = dungeon;
+            currentRoom = dungeon.startRoom;
+        }
+
+
+        //Misc methods
+
+        public bool CanUseEdge(VagueDungeonEdge edge)
+        {
+            //Returns if the explorer can use the given edge.
+
+            return false;
+        }
+
+        public VagueDungeonNode[] UseableNeighboringEdges()
+        {
+            //Returns all of the neighbors that can 
+
+            return null;
+        }
+    }
+}

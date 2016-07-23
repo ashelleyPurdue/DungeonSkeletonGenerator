47cd2f9c896db1dd34b999a324b10aea4899ac00
Alex Shelley <ashelley@purdue.edu>
2016-07-23 18:15:21 -0400

    Added the Utils folder to the solution.

---
+++ DungeonSkeletonGenerator/DefaultDictionary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonSkeletonGenerator.Utils
+{
+    public class DefaultDictionary<TKey, TVal>
+    {
+        public TVal defaultVal { get; private set; }
+
+        private Dictionary<TKey, TVal> contents = new Dictionary<TKey, TVal>();
+
+        public TVal this[TKey key]
+        {
+            get
+            {
+                EnsureExistance(key);
+                return contents[key];
+            }
+
+            set
+            {
+                EnsureExistance(key);
+                contents[key] = value;
+            }
+        }
+
+        public DefaultDictionary(TVal defaultVal)
+        {
+            this.defaultVal = defaultVal;
+        }
+
+        private void EnsureExistance(TKey key)
+        {
+            //Ensures a key exists
+            if (!contents.ContainsKey(key))
+            {
+                contents.Add(key, defaultVal);
+            }
+        }
+    }
+}

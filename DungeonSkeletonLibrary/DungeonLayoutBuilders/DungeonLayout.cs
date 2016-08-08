d97d66511dbdfaa3647bf98473eba0186d366dc3
Alex Shelley <ashelley@purdue.edu>
2016-08-08 16:06:16 -0400

    Started on DungeonLayout.
    Started on RectangleRoomsBuilder.BuildInternal()

--- DungeonSkeletonLibrary/DungeonLayoutBuilders/DungeonLayout.cs
+++ DungeonSkeletonLibrary/DungeonLayoutBuilders/DungeonLayout.cs
@@ -7,6 +7,19 @@ namespace DungeonSkeletonLibrary.DungeonLayoutBuilders
 {
     public class DungeonLayout
     {
-        //TODO: Store things related to the dungeon layout
+        //Store things related to the dungeon layout
+
+        public LayoutRoom this[int i]
+        {
+            get { return rooms[i]; }
+        }
+
+        private List<LayoutRoom> rooms = new List<LayoutRoom>();
+
+
+        public void AddRoom(LayoutRoom room)
+        {
+            //TODO: Add a room.
+        }
     }
 }
--- DungeonSkeletonLibrary/DungeonLayoutBuilders/RectangularRoomsBuilder.cs
+++ DungeonSkeletonLibrary/DungeonLayoutBuilders/RectangularRoomsBuilder.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using DungeonSkeletonLibrary.Utils;
 using DungeonSkeletonLibrary.VagueDungeons;
 
 namespace DungeonSkeletonLibrary.DungeonLayoutBuilders
 {
     public class RectangularRoomsBuilder : AbstractLayoutBuilder
     {
-        public delegate RectangleLayoutRoom RectangularRoomProviderMethod();
+        public delegate RectangleLayoutRoom RectangularRoomProviderMethod(DungeonRoom dngRoom);
         private RectangularRoomProviderMethod GetRoom;
+
+        private DefaultDictionary<DungeonRoom, bool> visited = new DefaultDictionary<DungeonRoom, bool>(false);
+        private Queue<DungeonRoom> bfsQueue = new Queue<DungeonRoom>();
 
         public RectangularRoomsBuilder(Dungeon vagueDungeon, RectangularRoomProviderMethod GetRoom) : base(vagueDungeon)
         {
@@ -20,6 +24,19 @@ namespace DungeonSkeletonLibrary.DungeonLayoutBuilders
         protected override void BuildInternal()
         {
             //TODO: Build the dungeon.
+
+            //Start the BFS
+            RectangleLayoutRoom startRoom = GetRoom(vagueDungeon.startRoom);
+            visited[vagueDungeon.startRoom] = true;
+            bfsQueue.Enqueue(vagueDungeon.startRoom);
+
+            //Use breadth first search to lay out every room.
+            while (bfsQueue.Count > 0)
+            {
+                //TODO: Advance the BFS
+            }
         }
+
+
     }
 }

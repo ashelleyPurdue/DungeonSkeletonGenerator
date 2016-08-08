90f2921d20a5fdf53e98aa6f3957b851d0038871
Alex Shelley <ashelley@purdue.edu>
2016-08-08 15:53:23 -0400

    Started working on the layout builder.
    Created the Vector struct to kick things off.

---
+++ DungeonSkeletonLibrary/Utils/Vector.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DungeonSkeletonLibrary.Utils
+{
+    public struct Vector
+    {
+        public static Vector zero = new Vector(0, 0, 0);
+        
+        double x;
+        double y;
+        double z;
+
+        public Vector(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public static Vector operator +(Vector a, Vector b)
+        {
+            return new Vector(a.x + b.x, a.y + b.y, a.z + b.z);
+        }
+
+        public static Vector operator -(Vector a, Vector b)
+        {
+            return a + (b * -1);
+        }
+
+        public static Vector operator *(Vector a, double b)
+        {
+            return new Vector(a.x * b, a.y * b, a.z * b);
+        }
+
+        public static Vector operator /(Vector a, double b)
+        {
+            return a * (1.0 / b);
+        }
+    }
+}

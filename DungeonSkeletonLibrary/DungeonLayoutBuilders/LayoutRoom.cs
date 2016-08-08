e32e596d0c39b75d6645d25971a5ee82464d9da2
Alex Shelley <ashelley@purdue.edu>
2016-08-08 16:06:16 -0400

    Started on LayoutRoom.  LayoutRoom will represent a DungeonRoom in space.

---
+++ DungeonSkeletonLibrary/DungeonLayoutBuilders/LayoutRoom.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using DungeonSkeletonLibrary.Utils;
+using DungeonSkeletonLibrary.VagueDungeons;
+
+namespace DungeonSkeletonLibrary.DungeonLayoutBuilders
+{
+    public class LayoutRoom
+    {
+        public LayoutRoom parent = null;
+        public DungeonRoom dungeonRoom;
+
+        public Vector localPosition = Vector.zero;
+        public Vector position
+        {
+            get
+            {
+                if (parent == null)
+                {
+                    return localPosition;
+                }
+                else
+                {
+                    return parent.position + localPosition;
+                }
+            }
+        }
+
+        public Vector size = Vector.zero;
+
+        public LayoutRoom(DungeonRoom dungeonRoom)
+        {
+            this.dungeonRoom = dungeonRoom;
+        }
+    }
+}
